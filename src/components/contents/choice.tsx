6251ce98ebfd999bfbfae83050e35753339e694b
t-noguchi <t-noguchi@isp.co.jp>
2020-04-23 12:57:17 +0900

    modified choice page

--- src/components/contents/choice.tsx
+++ src/components/contents/choice.tsx
@@ -1,6 +1,7 @@
 import { Button, Col, Row, Spin } from 'antd';
 import * as React from 'react';
 import { ArtistUid, MainProps, SongUid } from '../../models/Main';
+// import * as R from 'ramda';
 
 const colStyle: React.CSSProperties = { margin: 10, fontSize: 'large' };
 const buttonStyle: React.CSSProperties = { width: 400, fontSize: 20 };
@@ -13,6 +14,7 @@ interface State {
     [x: string]: { prefer: SongUid[]; same: SongUid[]; dislike: SongUid[] };
   };
   asked: Array<[SongUid, SongUid]>;
+  finished: boolean;
 }
 
 const initial: State = {
@@ -21,6 +23,7 @@ const initial: State = {
   targetList: [],
   result: {},
   asked: [],
+  finished: false,
 };
 
 const Choice = (props: MainProps<ArtistUid>) => {
@@ -48,11 +51,12 @@ const Choice = (props: MainProps<ArtistUid>) => {
     const check = () => {
       let toRemove: SongUid[] = [];
       Object.entries(result).forEach(([key, song]) => {
-        if (song.prefer.length > 10) {
+        if (song.prefer.length >= 10) {
           toRemove = song.same.concat(song.dislike).concat(key);
-        } else if (song.prefer.length + song.same.length > 10) {
-          toRemove = song.dislike;
         }
+        // else if (song.prefer.length + song.same.length >= 10) {
+        //   toRemove = song.dislike;
+        // }
       });
       nextList = nextList.filter(s => !toRemove.includes(s));
       result = Object.entries(result).reduce(
@@ -60,31 +64,89 @@ const Choice = (props: MainProps<ArtistUid>) => {
         {}
       );
     };
+
+    const like = (better: string, worse: string) => {
+      console.log(better, worse);
+
+      if (Object.keys(result).includes(better)) {
+        result[better].dislike.push(worse);
+        if (Object.keys(result).includes(worse)) {
+          result[better].dislike.push(...result[worse].same.filter(s => s !== better));
+          result[better].dislike.push(...result[worse].dislike.filter(s => s !== better));
+        }
+      } else {
+        result[better] = { prefer: [], same: [], dislike: [worse] };
+        if (Object.keys(result).includes(worse)) {
+          result[better].dislike.push(...result[worse].same.filter(s => s !== better));
+          result[better].dislike.push(...result[worse].dislike.filter(s => s !== better));
+        }
+      }
+      if (Object.keys(result).includes(worse)) {
+        result[worse].prefer.push(better);
+        if (Object.keys(result).includes(better)) {
+          result[worse].prefer.push(...result[better].same.filter(s => s !== worse));
+          result[worse].prefer.push(...result[better].dislike.filter(s => s !== worse));
+        }
+      } else {
+        result[worse] = { prefer: [better], same: [], dislike: [] };
+        if (Object.keys(result).includes(better)) {
+          result[worse].prefer.push(...result[better].same.filter(s => s !== worse));
+          result[worse].prefer.push(...result[better].dislike.filter(s => s !== worse));
+        }
+      }
+    };
+
     const localA = localState.targetA as string;
     const localB = localState.targetB as string;
     let nextA = localState.targetA;
     let nextB = localState.targetB;
     let nextList = localState.targetList;
+    let nextFinished = false;
     let result: State['result'] = { ...localState.result };
+
     if (value === 'A') {
-      if (Object.keys(result).includes(localA)) result[localA].dislike.push(localB);
-      else result[localA] = { prefer: [], same: [], dislike: [localB] };
-      if (Object.keys(result).includes(localB)) result[localB].prefer.push(localA);
-      else result[localB] = { prefer: [localA], same: [], dislike: [] };
+      like(localA, localB);
       check();
-      if (nextList.length > 0) nextA = randomChoice(nextList, [nextB, localA]);
+      if (nextList.length > 0) {
+        if (!nextList.includes(nextB)) nextB = randomChoice(nextList);
+        nextA = randomChoice(nextList, [nextB, localA]);
+      }
     } else if (value === 'B') {
-      if (Object.keys(result).includes(localB)) result[localB].dislike.push(localA);
-      else result[localB] = { prefer: [], same: [], dislike: [localA] };
-      if (Object.keys(result).includes(localA)) result[localA].prefer.push(localB);
-      else result[localA] = { prefer: [localB], same: [], dislike: [] };
+      like(localB, localA);
       check();
-      if (nextList.length > 0) nextB = randomChoice(nextList, [nextA, localB]);
+      if (nextList.length > 0) {
+        if (!nextList.includes(nextA)) nextA = randomChoice(nextList);
+        nextB = randomChoice(nextList, [nextA, localB]);
+      }
     } else if (value === 'C') {
-      if (Object.keys(result).includes(localA)) result[localA].same.push(localB);
-      else result[localA] = { prefer: [], same: [localB], dislike: [] };
-      if (Object.keys(result).includes(localB)) result[localB].same.push(localA);
-      else result[localB] = { prefer: [], same: [localA], dislike: [] };
+      if (Object.keys(result).includes(localA)) {
+        result[localA].same.push(localB);
+        if (Object.keys(result).includes(localB)) {
+          result[localA].same.push(...result[localB].same.filter(s => s !== localA));
+        }
+      } else {
+        result[localA] = { prefer: [], same: [localB], dislike: [] };
+        if (Object.keys(result).includes(localB)) {
+          result[localA].same.push(...result[localB].same.filter(s => s !== localA));
+        }
+      }
+      if (Object.keys(result).includes(localB)) {
+        result[localB].same.push(localA);
+        if (Object.keys(result).includes(localA)) {
+          result[localB].same.push(...result[localA].same.filter(s => s !== localB));
+        }
+      } else {
+        result[localB] = { prefer: [], same: [localA], dislike: [] };
+        if (Object.keys(result).includes(localA)) {
+          result[localB].same.push(...result[localA].same.filter(s => s !== localB));
+        }
+      }
+      check();
+      if (nextList.length > 0) {
+        if (!nextList.includes(nextA)) nextA = randomChoice(nextList);
+        if (!nextList.includes(nextB)) nextB = randomChoice(nextList);
+        nextB = randomChoice(nextList, [nextA, localB]);
+      }
     } else if (value === 'D') {
       nextList = nextList.filter(id => id !== localA);
       if (nextList.length > 0) nextA = randomChoice(nextList, [nextB]);
@@ -101,19 +163,23 @@ const Choice = (props: MainProps<ArtistUid>) => {
     while (nextList && (localState.asked.includes([nextA, nextB]) || localState.asked.includes([nextB, nextA]))) {
       nextB = randomChoice(nextList, [nextB]);
     }
+
+    if (nextList.length < 1) nextFinished = true;
+
     setLocalState({
       targetA: nextA,
       targetB: nextB,
       result: result,
       targetList: nextList,
       asked: ([[localState.targetA, localState.targetB]] as Array<[SongUid, SongUid]>).concat(localState.asked),
+      finished: nextFinished,
     });
   };
 
   return props.content && props.content.songs && localState.targetA && localState.targetB ? (
     <Row style={{ textAlign: 'center', overflowY: 'auto' }}>
       {console.log(localState)}
-      {localState.targetList.length > 0 ? (
+      {!localState.finished ? (
         <>
           <Col style={colStyle}>好きな方を選んで下さい</Col>
           <Col style={colStyle}>A: 「{props.content.songs[localState.targetA as string].name}」</Col>
@@ -135,19 +201,20 @@ const Choice = (props: MainProps<ArtistUid>) => {
           </Col>
           <Col style={colStyle}>
             <Button type="primary" style={buttonStyle} onClick={() => select('D')}>
-              Aを知らない
+              Aを除外する
             </Button>
           </Col>
           <Col style={colStyle}>
             <Button type="primary" style={buttonStyle} onClick={() => select('E')}>
-              Bを知らない
+              Bを除外する
             </Button>
           </Col>
           <Col style={colStyle}>
             <Button type="primary" style={buttonStyle} onClick={() => select('F')}>
-              両方知らない
+              両方除外する
             </Button>
           </Col>
+          <Col style={colStyle}>「除外する」 ... 知らない／思い出せない／絶対に入らない</Col>
           <Col style={colStyle}>
             最大であと {(localState.targetList.length * (localState.targetList.length - 1)) / 2} 回
           </Col>
